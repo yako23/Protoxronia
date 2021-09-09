6af7dca7748f65704001667cc842560d7960a588
Yannis <yanniskoutras@hotmail.com>
2021-09-09 20:47:54 +0300

    initial commit

---
+++ src/com/example/protoxronia/Protoxronia.java
@@ -0,0 +1,17 @@
+package com.example.protoxronia;
+
+import java.util.Scanner;
+
+public class Protoxronia {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("dwse arithmo 1:");
+        int a = scan.nextInt();
+
+        System.out.println("dwse arithmo 2:");
+        int b = scan.nextInt();
+
+        System.out.println("to athroisma twn 2 arithmwn einai:");
+        System.out.println(a+b);
+    }
+}
